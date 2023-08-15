f0d27387fc3aaadcea7033d5c790289cbf9b318b
Anup <anantpatankar1@icloud.com>
2023-08-16 02:55:01 +0530

    Call By Value, Address & References

---
+++ Essential c and c++ concepts/callByAddress.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main(){
+    int num1,num2;
+    num1 = 10;
+    num2 = 8;
+    cout<<"Call by Address change in variable"<<endl;
+    cout<<"Before Swapping "<<num1<<" "<<num2<<endl;
+    swap(&num1,&num2);
+    cout<<"After Swapping "<<num1<<" "<<num2<<endl;
+}
