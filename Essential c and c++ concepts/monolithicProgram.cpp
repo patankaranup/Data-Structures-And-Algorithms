8de86bf1989b03a5c147c950c344b7016239bb0f
Anup <anantpatankar1@icloud.com>
2023-08-20 17:12:59 +0530

    Monolithic Program

---
+++ Essential c and c++ concepts/monolithicProgram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+    int len = 0;
+    int breadth = 0;
+    cout<<"Enter Len & breadth "<<endl;;
+    cin>>len>>breadth;
+    int area = len*breadth;
+    cout<<"Area is "<<area<<endl;
+
+    return 0;
+}
