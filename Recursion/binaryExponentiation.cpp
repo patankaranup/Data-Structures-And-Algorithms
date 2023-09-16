0b1203666caa9bd6c2505f904892b65f3c37c2ae
Anup <anantpatankar1@icloud.com>
2023-09-17 01:04:41 +0530

    Binary Exponentiation Iterative Approach

--- Recursion/binaryExponentiation.cpp
+++ Recursion/binaryExponentiation.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
 using namespace std;
-int pow(int m, int n){
+int powR(int m, int n){
     if(n==0){
         return 1;
     }
     if(n%2 == 0){
-        return pow(m*m,n/2);
+        return powR(m*m,n/2);
     } else {
-        return m * pow(m*m,(n-1)/2);
+        return m * powR(m*m,(n-1)/2);
     }
 }
+int powIter(int m, int n){
+    int ans = 1;
+
+    while(n>0){
+        // odd number 
+        if(n%2 == 1){
+            ans = ans * m;
+            m = m*m;
+        }
+        else {
+            m = m * m;
+        }
+        n /= 2;
+    }
+    return ans;
+}
 int main(){
-    int ans = pow(3,1000);
-    cout<<ans<<endl;
+    int ans1 = powR(3,1000);
+    int ans2 = powIter(3,1000);
+    cout<<ans1<<endl;
+    cout<<ans2<<endl;
     return 0;
 }
