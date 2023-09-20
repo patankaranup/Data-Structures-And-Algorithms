04385ffc222d2fc031d97dd6655692b8442e6f5a
Anup <anantpatankar1@icloud.com>
2023-09-21 01:21:50 +0530

    Fibonacci Series Recursion and Memoization

--- Recursion/fibonacciSeries.cpp
+++ Recursion/fibonacciSeries.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 using namespace std;
+int memo[10];
 int fib(int n){
     if(n<=1){
+        memo[n] = n;
         return n;
+    } else {
+        if(memo[n-2] == -1){
+            memo[n-2] = fib(n-2);
+        } 
+        if(memo[n-1] == -1){
+            memo[n-1] = fib(n-1);
+        }
+        return memo[n] = memo[n-2] + memo[n-1];
     }
-    return fib(n-1) + fib(n-2);
 }
 int main(){
-    int ans = fib(7);
+    for(int i=0; i<10; i++){
+        memo[i] = -1;
+    }
+    int ans = fib(10);
     cout<<ans<<endl;
     return 0;
 }
