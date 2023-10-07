48757757022d199b167029b7c8457185a3a7b92c
Anup <anantpatankar1@icloud.com>
2023-10-08 03:14:19 +0530

    Append and Insert Function ADT

--- Array ADT/main.cpp
+++ Array ADT/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 struct Array{
-    int A[20];
+    int A[10];
     int size;
     int length;
     
@@ -16,8 +16,25 @@ void Display(struct Array &arr){
     cout<<endl;
 }
 
+void Append(struct Array &arr, int x){
+    if(arr.length < arr.size){
+        arr.A[arr.length] = x;
+        arr.length++;
+    }
+}
+
+void Insert(struct Array &arr, int index, int x){
+    if(index>=0 && index<=arr.length){
+        for(int i=arr.length; i>index; i--){
+            arr.A[i] = arr.A[i-1];
+        }
+        arr.A[index] = x;
+    }
+}
 int main(){
-    struct Array arr = {{2,4,6,8,0},20,5};
+    struct Array arr = {{2,4,6,8,0},10,5};
+    Append(arr,10);
+    Insert(arr,0,9);
     Display(arr);
     return 0;
 }
