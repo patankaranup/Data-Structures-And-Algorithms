66949f71e3ca94476ed41d683e10b5d642b73e9f
Anup <anantpatankar1@icloud.com>
2023-10-10 00:36:13 +0530

    Linear Search Array ADT

--- Array ADT/main.cpp
+++ Array ADT/main.cpp
@@ -45,9 +45,28 @@ int Delete(struct Array &arr, int index){
     return 0;
 }
 
+void swap(int &x, int &y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int LinearSearch(struct Array &arr, int key){
+    for(int i=0; i<arr.length; i++){
+        if(arr.A[i] == key){
+            // Using Transposition 
+            // swap(arr.A[i],arr.A[i-1]);
+            // Moving To front
+            swap(arr.A[i],arr.A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main(){
     struct Array arr = {{2,4,6,8,0},10,5};
-    cout<<Delete(arr,0)<<endl;
+    cout<<LinearSearch(arr,8)<<endl;
     Display(arr);
     return 0;
 }
